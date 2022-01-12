28850d6e5835026c7346e5ee9002a13ac84cf875
piotr233 <piotr.drabik@edu.uni.lodz.pl>
2022-01-12 16:33:42 +0100

    Try to exit after print

--- pronterface_interface.py
+++ pronterface_interface.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def spawn_and_send_commands(pronterface_path, gcode_path):
-    sub = subprocess.run([pronterface_path, "-v", "-e connect COM4" , "-e block_until_online" , f"-e load {gcode_path}", "-e print"])
+    sub = subprocess.run([pronterface_path, "-v", "-e connect COM4" , "-e block_until_online" , f"-e load {gcode_path}", "-e print", "-e exit"])
 
 if __name__ == '__main__':
     spawn_and_send_commands("C:/I/Printrun-Win-Slic3r-03Feb2015/pronsole.exe", "test.gcode")
