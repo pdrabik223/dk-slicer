3726dc9b6e6f7cd033a122d79e38aade5e4a8407
piotr233 <piotr.drabik@edu.uni.lodz.pl>
2022-01-12 06:48:04 +0100

    Adding support for launching prints directly from python

--- pronterface_interface.py
+++ pronterface_interface.py
@@ -1,12 +1,7 @@
 import subprocess
 
+def spawn_and_send_commands(pronterface_path, gcode_path):
+    sub = subprocess.run([pronterface_path, "-v", "-e connect COM4" , "-e block_until_online" , f"-e load {gcode_path}", "-e print"])
 
-def spawn_and_send_commands(pronterface_path, commands):
-    with subprocess.Popen([pronterface_path],
-                          stdin=subprocess.PIPE,
-                          stdout=subprocess.PIPE) as proc:
-        for command in commands:
-            while not proc.stdin.writable():
-                print(proc.stdout.readlines())
-            
-            proc.stdin.writelines([command])
+if __name__ == '__main__':
+    spawn_and_send_commands("C:/I/Printrun-Win-Slic3r-03Feb2015/pronsole.exe", "test.gcode")
