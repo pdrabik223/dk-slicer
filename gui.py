94f8c94f9b02206527c7cf9824c307131426715a
Mateusz Kojro <mateuszkojro@outlook.com>
2022-01-12 05:04:41 +0100

    Adding simple gui

---
+++ gui.py
@@ -0,0 +1,68 @@
+import sys
+
+import PyQt5
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtGui import QPixmap
+
+from slicer_devel import convert_img
+
+class App():
+    def __init__(self) -> None:
+        self.app = QApplication(sys.argv)
+        self.window = QWidget()
+        self.window.setWindowTitle('DK-Plotter')
+        self.window.setGeometry(640, 480, 280, 80)
+
+        layout = QVBoxLayout()
+
+        self.img_widget = QLabel()
+        self.image_path = "./assets/lena.jpg" 
+        self.set_image(self.image_path)
+
+        layout.addWidget(self.img_widget)
+
+        load_image_button = QPushButton('Load image')
+        load_image_button.clicked.connect(self.load_image_clicked)
+        layout.addWidget(load_image_button)
+
+        convert_button = QPushButton('Convert')
+        convert_button.clicked.connect(self.convert_image_clicked)
+        layout.addWidget(convert_button)
+
+        print_button = QPushButton('Print')
+        # print_button.clicked.connect(print_image)
+        layout.addWidget(print_button)
+
+        self.window.setLayout(layout)
+
+    def load_image_clicked(self):
+        fname, extensions = QFileDialog.getOpenFileName(self.window, 'Open file',  'c:\\',"Image files (*.jpg *.gif *.png *.bnp)")
+        if fname != "":
+            self.image_path = fname
+            self.set_image(fname)
+
+    def convert_image_clicked(self):
+        fname = convert_img(self.image_path, "tmp.gcode")
+        self.set_image(fname)
+
+
+    def set_image(self, path):
+        self.image_path = path
+        pixmap = QPixmap(path)
+        pixmap = pixmap.scaled(640, 480, PyQt5.QtCore.Qt.KeepAspectRatio)
+        self.img_widget.setPixmap(pixmap)
+
+    def run(self):
+        self.window.show()
+        return self.app.exec_()
+
+
+if __name__ == '__main__':
+    app = App()
+    sys.exit(app.run())
